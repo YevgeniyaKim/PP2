f2ed0f2f7cba5377da9bb129ee6478801b3eef85
Yevgeniya <ekim27220@gmail.com>
2019-03-02 13:07:32 +0600

    lab4

---
+++ Week5/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    public class Complex
+    {
+        //[XmlIgnore]
+        public int real;
+        public int imaginary;
+        public Complex() { }
+
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public override string ToString()
+        {
+            return (string.Format("{0}+{1}i", real, imaginary));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Complex c = new Complex(a, b);
+            FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            xs.Serialize(fs, c);
+            fs.Close();
+
+            FileStream fs1 = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs1 = new XmlSerializer(typeof(Complex));
+            Complex c1 = xs1.Deserialize(fs1) as Complex;
+            Console.WriteLine(c1);
+            fs1.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week5/Task2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task2
+{
+    public class MarkList
+    {
+        //создаем лист оценок(буквенных)
+        public List<Marks> marks = new List<Marks>();
+        //сериализация, десериализация
+        public void Save(MarkList m)
+        {
+            FileStream fs = new FileStream("marks.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(MarkList));
+            xs.Serialize(fs, m);
+            fs.Close();
+        }
+
+        public void Show()
+        {
+            FileStream fs = new FileStream("marks.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(MarkList));
+            MarkList m = xs.Deserialize(fs) as MarkList;
+            for (int i = 0; i < m.marks.Count; i++)
+            {
+                Console.WriteLine(m.marks[i]);
+            }
+        }
+    }
+
+    public class Marks
+    {
+        public double point;//баллы
+        public Marks() { }//массив оценок
+
+        public Marks(double point)
+        {
+            this.point = point;
+        }
+
+        public string getLetter()//массив буквенных обозначений
+        {
+            if (point >= 95 && point <=100) return "A";
+            if (point >= 90 && point < 95) return "A-";
+            if (point >= 85 && point < 90) return "B+";
+            if (point >= 80 && point < 85) return "B";
+            if (point >= 75 && point < 80) return "B-";
+            if (point >= 70 && point < 75) return "C+";
+            if (point >= 65 && point < 70) return "C";
+            if (point >= 60 && point < 65) return "C-";
+            if (point >= 55 && point < 60) return "D+";
+            if (point >= 50 && point < 55) return "D";
+            return "F";
+        }
+
+        public override string ToString()//метод вывода оценки
+        {
+            return "Mark is " + this.point + " " + this.getLetter();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //пишем оценки и добавляем в лист М
+            Marks a = new Marks(67);
+            Marks b = new Marks(89);
+            Marks c = new Marks(94);
+
+            MarkList M = new MarkList();
+            M.marks.Add(a);
+            M.marks.Add(b);
+            M.marks.Add(c);
+            //вызываем методы
+            M.Save(M);
+            M.Show();
+            Console.ReadKey();
+
+        }
+    }
+}
