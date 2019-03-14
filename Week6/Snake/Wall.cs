7e46d6bee58b2d2fda6256eecf8da89dbf8074b3
Yevgeniya <ekim27220@gmail.com>
2019-03-14 22:34:30 +0600

    lab6

---
+++ Week6/Snake/Wall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Wall:GameObject
+    {
+        public Wall(char sign) : base(sign)
+        {
+            LoadLevel(2);
+        }
+
+        public void LoadLevel(int level)
+        {
+            string name = string.Format("Levels/Level{0}.txt", level);
+            using (StreamReader sr = new StreamReader(name))
+            {
+                int r = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    for(int c = 0; c < line.Length; c++)
+                    {
+                        if (line[c] == '#')
+                        {
+                            body.Add(new Point
+                            {
+                                X = c,
+                                Y = r
+                            });
+                        }
+                    }
+                    r++;
+                }
+            }
+        }
+    }
+}
