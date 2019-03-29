f33d04bebec34ee926cc998c90239abdfeed8497
Yevgeniya <ekim27220@gmail.com>
2019-03-29 06:52:27 +0600

    TSIS6

---
+++ Week7/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        class MyThread
+        {
+            Thread thread;
+            string Name;
+
+            public MyThread(string name) 
+            {
+                Name = name;
+            }
+
+            public void StartThread()
+            {
+                thread = new Thread(Output);
+                thread.Start();
+            }
+
+            public void Output()
+            {
+                int i = 0;
+
+                while (i != 4)
+                {
+                    i++;
+                    Console.WriteLine((Name + " " + "выводит" + " " + i).ToString());
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine(Name + " " + "завершился");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyThread t1 = new MyThread("Thread 1");
+            MyThread t2 = new MyThread("Thread 2");
+            MyThread t3 = new MyThread("Thread 3");
+
+            t1.StartThread();
+            t2.StartThread();
+            t3.StartThread();
+
+            Console.Read();
+        }
+    }
+
+    
+}
---
+++ Week7/Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] thread = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                thread[i] = new Thread(Func);
+                thread[i].Name = ("Thread " + i).ToString();
+                thread[i].Start();
+            }
+
+        }
+
+        private static void Func()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " выводит " + i);
+            }
+        }
+    }
+}
